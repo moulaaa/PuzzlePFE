af663382408ffe96ab8d1f2e00cb4a4527ee03ae
Mouadh Mkadmi <wvagen@gmail.com>
2019-06-19 21:54:49 +0100

    Version 15.0

--- Assets/scripts/MainMenuHandler.cs
+++ Assets/scripts/MainMenuHandler.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class MainMenuHandler : MonoBehaviour
 {
     public Text finalScoreTxt;
     public static bool soundEnabled = true;
-    public Image soundBtnImg;
+    public static bool musicEnabled = true;
+    public Image soundBtnImg,musicBtnImg;
 
     public Sprite soundOnSprite, soundOffSprite;
+    public Sprite musicOnSprite, musicOffSprite;
+
 
     void Start()
     {
-        finalScoreTxt.text = PlayerPrefs.GetInt("score", 0).ToString();
+      if (finalScoreTxt != null)  finalScoreTxt.text = PlayerPrefs.GetInt("score", 0).ToString();
         soundEnabled = PlayerPrefs.GetInt("sound", 0) == 0;
+        soundEnabled = PlayerPrefs.GetInt("music", 0) == 0;
+
         if (soundEnabled)
         {
             soundEnabled = false;
@@ -24,6 +30,27 @@ public class MainMenuHandler : MonoBehaviour
             soundEnabled = true;
             soundBtnImg.sprite = soundOnSprite;
         }
+        if (musicEnabled)
+        {
+            musicEnabled = false;
+            musicBtnImg.sprite = musicOffSprite;
+        }
+        else
+        {
+            musicEnabled = true;
+            musicBtnImg.sprite = musicOnSprite;
+        }
+
+    }
+
+    public void PlayBtn()
+    {
+        SceneManager.LoadScene("Selection Scene");
+    }
+
+    public void ExitBtn()
+    {
+        Application.Quit();
     }
 
     public void EnableDisableSound()
@@ -41,4 +68,26 @@ public class MainMenuHandler : MonoBehaviour
             PlayerPrefs.SetInt("sound", 1);
         }
     }
+    public void EnableDisableMusic()
+    {
+        if (musicEnabled)
+        {
+            musicEnabled = false;
+            musicBtnImg.sprite = musicOffSprite;
+            PlayerPrefs.SetInt("music", 0);
+        }
+        else
+        {
+            musicEnabled = true;
+            musicBtnImg.sprite = musicOnSprite;
+            PlayerPrefs.SetInt("music", 1);
+        }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+
+    }
+
 }
